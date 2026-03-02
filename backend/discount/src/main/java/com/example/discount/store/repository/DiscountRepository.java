8c8ddbcdc1fad9d6248e2cf788628eff15bdddeb
727823tuit143-prog <727823tuit143@skct.edu.in>
2026-03-02 22:50:10 +0530

    commit

--- backend/discount/src/main/java/com/example/discount/store/repository/DiscountRepository.java
+++ backend/discount/src/main/java/com/example/discount/store/repository/DiscountRepository.java
@@ -1,7 +1,7 @@
 package com.example.discount.store.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.discount.entity.store.Discount;
+import com.example.discount.store.entity.Discount;
 import java.time.LocalDate;
 import java.util.List;
 
